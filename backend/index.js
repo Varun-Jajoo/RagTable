0faf81d2bd39c4f9240eae4ee9b30e919aaa7b09
Shreyans jain <shreyansjain1603@gmail.com>
2024-07-17 02:45:37 +0530

    backend created

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,77 @@
-import { ChromaClient } from 'chromadb'
+import { ChromaClient } from "chromadb";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 const client = new ChromaClient();
 
-const  main = async()=> {
-    const collection = await client.getOrCreateCollection({
-        name: "my_collection",
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const main = async (text, queryTexts) => {
+  const collection = await client.getOrCreateCollection({
+    name: "my_collection",
+  });
+
+  function splitTextByFullStop(text) {
+    let sentences = text.split(/\. ?/);
+    sentences = sentences.map((sentence, index) => {
+      if (
+        index !== sentences.length - 1 ||
+        (index === sentences.length - 1 && sentence !== "")
+      ) {
+        return sentence + ".";
+      }
+      return sentence;
     });
-    
-    // switch `add` to `upsert` to avoid adding the same documents every time
-    await collection.upsert({
-        documents: [
-            "This is a document about pineapple",
-            "This is a document about oranges"
-        ],
-        ids: ["id1", "id2"],
-    });
-    
-    const results = await collection.query({
-        queryTexts: ["oranges"], 
-        nResults: 1, 
-    });
-    
-    console.log(results)
-    
-}
-main();
+    if (sentences[sentences.length - 1] === "") {
+      sentences.pop();
+    }
+
+    return sentences;
+  }
+
+  function generateIds(documents) {
+    return documents.map((_, index) => `id${index + 1}`);
+  }
+
+  const documents = splitTextByFullStop(text);
+  const ids = generateIds(documents);
+
+  await collection.upsert({
+    documents,
+    ids,
+  });
+
+  const results = await collection.query({
+    queryTexts,
+    nResults: 2,
+  });
+  return results;
+};
+
+app.post("/data", async (req, res) => {
+  const { fields, docText } = req.body;
+
+  console.log("Fields:", fields);
+  console.log("Document Text:", docText);
+
+  const description = () => {
+    return fields.map((field) => `${field.description}`);
+  }
+  const queryTexts = description();
+
+  try {
+    const results = await main(docText, queryTexts);
+    console.log("Query Results:", results.documents);
+    res.status(200).json({ message: "Data received successfully", documents: results.documents,fields });
+  } catch (error) {
+    console.error("Error during query:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+app.listen(8080, () => {
+  console.log("Listening on port 8080");
+});
